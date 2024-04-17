28f75ab279ee694c4ebdaee34641b5a03ebfd811
Pablo Santos <opablosantos@gmail.com>
2024-04-16 23:15:37 -0300

    feat(p1): obtem coords a partir de cidade

---
+++ consultaCoordenadas.js
@@ -0,0 +1,19 @@
+const axios = require ("axios")
+require("dotenv").config()
+
+const {
+    APPID,
+    CITY_NAME,
+    URL_BASE
+} = process.env
+
+const url = `${URL_BASE}?q=${CITY_NAME}&appid=${APPID}`
+
+axios.get(url)
+.then(res => {
+    return res.data
+})
+.then(res => {
+    console.log('Coordenadas de %s: Latitude: %d | Longitude: %d', res.city.name, res.city.coord.lat, res.city.coord.lon)
+    return res
+})
